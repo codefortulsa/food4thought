568b0f656d3ad1da836337cd03ba3fbe151b48ff
Vic Agbasi <vicagbasi@gmail.com>
2018-01-20 19:05:46 -0600

    almost there with the geojson

--- angularCS/src/app/map.service.ts
+++ angularCS/src/app/map.service.ts
@@ -3,19 +3,13 @@ import { Http } from '@angular/http';
 import { BehaviorSubject } from 'rxjs';
 import { Observable } from "rxjs/Observable";
 import { Router } from "@angular/router";
-import {
-    BBox, Feature, FeatureCollection, GeometryCollection, LineString, GeoJsonProperties
-    MultiLineString, MultiPoint, MultiPolygon, Point, Polygon, GeometryObject
-} from "geojson";
-// import { GenericGeoJSONFeature, GenericGeoJSONFeatureCollection } from '@yaga/generic-geojson';
-// import { TestInterface } from './myGeoJson';
 
 
 @Injectable()
 export class MapService {
   datasetToken: String = "cjcnxy2y11nq82pmr8qu3zu22"
   mapToken: String = "pk.eyJ1IjoidmljYWdiYXNpIiwiYSI6ImNqY2lpcWE2aTNteGEycWxscDl2NzhpZWQifQ.KJIJ5fZsHtxebZwwROAc5w"
-  mealSites: FeatureCollection<Point | LineString | Polygon | MultiPoint | MultiLineString | MultiPolygon | GeometryCollection> ;
+  mealSites: any ;
   mealSitesObservable = new BehaviorSubject(this.mealSites);
 
   constructor(private _http : Http, private _router : Router) { }
