1e14ca5bea06355a693634cd12732914e8e87cb7
Vic Agbasi <vicagbasi@gmail.com>
2018-02-02 09:36:11 -0600

    express server live now

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
 const express = require("express");
-const session =  require("express-session");
 const path = require("path");
-const bodyParser = require("body-parser");
-const PORT = 1895;
+
+const PORT = 2750;
 
 const app = express();
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, './angularCS/dist')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true }));
 
-app.use(session({secret: "asfasfsf@$#r$##4rgsoigm9fn408"}));
-
-// require('./server/config/mongoose');
-require('./server/config/routes')(app);
-
-app.get('/', (request, resp)=> {
-    resp.render('index');
-
-})
 
 app.listen(PORT, ()=> {
   console.log(`Listening on port: ${PORT}`)
