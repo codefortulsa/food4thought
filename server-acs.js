aea24ab8d56612793098d9dda80580cf737d67ca
Vic Agbasi <vicagbasi@gmail.com>
2018-02-15 11:51:08 -0600

    restructured files for heroku demo deployment

---
+++ server-acs.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.listen(PORT, ()=> {
+  console.log(`Listening on port: ${PORT}`)
+})
