5bb028576deffa4ab94d3c480c1ff0300654b964
Vic Agbasi <vicagbasi@gmail.com>
2018-02-15 12:06:42 -0600

    minor changes

--- server-acs.js
+++ server-acs.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname, '/dist'));
 
 app.listen(PORT, ()=> {
   console.log(`Listening on port: ${PORT}`)
