eaf464411b842509f1035ae76db4e997f7497be6
Vic Agbasi <vicagbasi@gmail.com>
2018-02-22 20:48:51 -0600

    ready to deploy. fingers crossed

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = 2750;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './angularCS/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(PORT, ()=> {
   console.log(`Listening on port: ${PORT}`)
