7c7ae9722b1a9278d18e867bdf9df39a42e51db6
VicAgbasi <vicagbasi@gmail.com>
2018-06-06 23:42:24 -0500

    new dataset with 590+ site listings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   datasetToken: "cjcnxy2y11nq82pmr8qu3zu22",
   mapToken: "pk.eyJ1IjoidmljYWdiYXNpIiwiYSI6ImNqY2lpcWE2aTNteGEycWxscDl2NzhpZWQifQ.KJIJ5fZsHtxebZwwROAc5w",
-  ms18: "cjhr0pf2200f11gqtx9yj6l2g"
+  ms18: "cji41y8b104la7qs0g68l6uzr"
 };
 
 // https://api.mapbox.com/datasets/v1/vicagbasi/cjhpcd14w0b76hgmhaezj6vzi/features?access_token=pk.eyJ1IjoidmljYWdiYXNpIiwiYSI6ImNqY2lpcWE2aTNteGEycWxscDl2NzhpZWQifQ.KJIJ5fZsHtxebZwwROAc5w
