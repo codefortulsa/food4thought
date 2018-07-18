c052c0208faf11d42da3420286bbfa02e9cc4e5b
VicAgbasi <vicagbasi@gmail.com>
2018-07-17 22:04:51 -0500

    minor changes. enforce https.

--- prodServer.js
+++ prodServer.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
+var http = require('http');
+var enforce = require('express-sslify');
 
 const PORT = process.env.PORT || 8080;
-const forceHTTPS = require("expressjs-force-https").forceHTTPS;
 const app = express();
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static(path.join(__dirname, './dist')));
-app.use(forceHTTPS);
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
